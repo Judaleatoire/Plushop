5410318cb746c6ba637c97742c314c79501fa2cf
François <81768287+Judaleatoire@users.noreply.github.com>
2022-05-27 23:44:56 +0200

    Delete defilement-titre.js

--- js/defilement-titre.js
+++
@@ -1,7 +0,0 @@
-var titres = document.querySelectorAll("h2");
-
-titres.forEach(element => {
-    element.addEventListener("scroll", function() {
-        element.style.left = window.scrollY * 5 + "px";
-    });
-});
